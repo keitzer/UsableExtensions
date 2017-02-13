0cb1bc79800dfc685ef0a2c18265923241d3a52a
Alex Ogorek <ogoalex57@gmail.com>
2017-02-13 06:48:59 -0500

    Initial Commit

---
+++ TestOpenSource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestOpenSource
+//
+//  Created by Alex Ogorek on 2/13/17.
+//  Copyright © 2017 XYZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
